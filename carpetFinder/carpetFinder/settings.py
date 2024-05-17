8a87fb461facacee507292d688a0da5a154dc860
26fati <fatibeautiful1@gmail.com>
2024-05-17 16:49:57 +0100

    setup the database

--- carpetFinder/carpetFinder/settings.py
+++ carpetFinder/carpetFinder/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'carpetFinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'pCWCRxyJqQPGbehwrbzlekDorbPxTjYH',
+        'HOST': 'monorail.proxy.rlwy.net',  
+        'PORT': '21594',
     }
 }
 
